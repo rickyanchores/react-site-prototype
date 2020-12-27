6314d2a45a7d024c894494f2172036ccdfca1e72
RickyAnchores <ricky23anchores@gmail.com>
2020-12-27 01:45:11 +0000

    final update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Header from "./components/Header";
 import Showcase from "./components/Showcase";
+import Main from "./components/Main";
+import Cards from "./components/Cards";
+import About from "./components/About";
+import Footer from "./components/Footer";
 import "./styles.css";
 
 export default function App(){
@@ -8,7 +12,10 @@ export default function App(){
          <div className="App">
              <Header />
              <Showcase />
-            
+             <Main />
+             <Cards />
+             <About />
+             <Footer />
          </div>
     )
 }
